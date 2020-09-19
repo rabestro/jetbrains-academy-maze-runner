be6bab9221da48c7d0c28eed5f5f6a2cb0a135ae
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-19 13:21:07 +0300

    Added a constructor for class Maze

--- Maze Runner/task/src/maze/Maze.java
+++ Maze Runner/task/src/maze/Maze.java
@@ -1,6 +1,12 @@
 package maze;
 
 public class Maze {
-    private int height;
-    private int width;
+    private final int height;
+    private final int width;
+
+
+    public Maze(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
 }
